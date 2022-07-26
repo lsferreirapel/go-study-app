f5161e401035446d8a7809877f96b541911faccf
Lucas Ferreira <lsferreirapel@gmail.com>
2022-07-26 01:05:21 -0300

    chore: update global types

---
+++ src/common/types/env.d.ts
@@ -0,0 +1,7 @@
+declare module "@env" {
+  export const API_URL: string;
+  export const API_WS_URL: string;
+  export const API_REPO: string;
+  export const WEB_URL: string;
+  export const STORAGE_PREFIX: string;
+}
