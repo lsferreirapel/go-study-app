41c414d238fd26379e1a8c1d3399a8e4dce17a20
Lucas Ferreira <lsferreirapel@gmail.com>
2022-07-26 02:28:48 -0300

    chore: improve modules imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          "@src": "./src",
+          "@common": "./src/common",
+          "@assets": "./src/common/assets",
+          "@navigators": "./src/navigators",
+          "@features": "./src/features",
+          "@generated": "./src/generated",
+        }
+      }
+    ],
+    ["module:react-native-dotenv"],    
+  ]
 };
