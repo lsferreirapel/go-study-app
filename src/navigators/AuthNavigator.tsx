a152fa5bf16777b4ee73cd2e8af512b3883df70d
Lucas Ferreira <lsferreirapel@gmail.com>
2022-07-31 01:16:35 -0300

    feat: add base form structure

--- src/navigators/AuthNavigator.tsx
+++ src/navigators/AuthNavigator.tsx
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { AppRoutes } from "@common/config/routes";
 import { IntroScreen } from "@src/screens/auth/IntroScreen";
+import { LoginScreen } from "@src/screens/auth/LoginScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -16,8 +17,9 @@ export function AuthNavigator() {
         headerShown: false,
         contentStyle: { backgroundColor: theme?.colors?.white },
       }}
-      initialRouteName={AppRoutes.AUTH.INTRO}>
+      initialRouteName={AppRoutes.AUTH.LOGIN}>
       <Stack.Screen name={AppRoutes.AUTH.INTRO} component={IntroScreen} />
+      <Stack.Screen name={AppRoutes.AUTH.LOGIN} component={LoginScreen} />
     </Stack.Navigator>
   );
 }
--- src/screens/auth/IntroScreen/index.tsx
+++ src/screens/auth/IntroScreen/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Div, Text } from "react-native-magnus";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export function IntroScreen() {
+  const { top, bottom, left, right } = useSafeAreaInsets();
+
   return (
-    <Div>
+    <Div pt={top} pb={bottom} pl={left} pr={right}>
       <Text>IntroScreen</Text>
     </Div>
   );
