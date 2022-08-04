58e17de4532e5cff3ea8fcb7b16495ea0d1d2e86
Lucas Ferreira <lsferreirapel@gmail.com>
2022-08-03 22:33:39 -0300

    chore: add auth screens assets

---
+++ src/common/assets/icons/index.ts
@@ -0,0 +1,5 @@
+import LogoFullIcon from "@assets/icons/logo-full.svg";
+import LogoFullColoredIcon from "@assets/icons/logo-full-color.svg";
+import LogoIcon from "@assets/icons/logo-icon.svg";
+
+export { LogoFullColoredIcon, LogoFullIcon, LogoIcon };
